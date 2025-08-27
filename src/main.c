ea33f13b6796dfd23850b489d96df8a01819a683
Yuzhao Shi <frank20050415@sjtu.edu.cn>
2025-08-28 01:46:31 +0800

    docs: revise how the verbose prints

--- src/main.c
+++ src/main.c
@@ -16,20 +16,30 @@ int main(int argc, char *argv[], char *envp[])
     }
     else if (in_args.help)
     {
-        printf(
-            "Welcome to use my shell!\n\nThis project is a mix of 15-213's "
-            "shell lab and VE482's project 1.\n\n");
-        printf("Currently the project is still largely unfinished,\nwith the "
-               "following features remain to be implemented:\n\n");
-        printf("3. Unix Pipes\n");
-        printf("4. Background Jobs and Signals\n");
-        printf("5. Built-in commands and expansion\n");
-        printf("6. Error handing and improved robustness\n");
-        printf("\n");
-        printf("Currently these features are already implemented:\n\n");
-        printf("0. (Kind of) functioning program that accepts 'exit'\n");
-        printf("1. Execute single external commands with flags\n");
-        printf("2. I/O Redirection and PATH resolution\n");
+        printf("Welcome to use my shell!\n");
+        if (in_args.verbose)
+        {
+            printf("\n");
+        }
+
+        printf("This project is a mix of 15-213's "
+               "shell lab and VE482's project 1.\n");
+
+        if (in_args.verbose)
+        {
+            printf("\n");
+            printf("Currently the project is still largely unfinished,\nwith the "
+                   "following features remain to be implemented:\n\n");
+            printf("3. Unix Pipes\n");
+            printf("4. Background Jobs and Signals\n");
+            printf("5. Built-in commands and expansion\n");
+            printf("6. Error handing and improved robustness\n");
+            printf("\n");
+            printf("Currently these features are already implemented:\n\n");
+            printf("0. (Kind of) functioning program that accepts 'exit'\n");
+            printf("1. Execute single external commands with flags\n");
+            printf("2. I/O Redirection and PATH resolution\n");
+        }
         return 0;
     }
 
@@ -58,7 +68,7 @@ int main(int argc, char *argv[], char *envp[])
         }
         eval(in_args, cmdline, args, cmd);
         exec(in_args, cmdline, args,
-             envp,cmd); // how can I setup environ array to setup the env var?
+             envp, cmd); // how can I setup environ array to setup the env var?
     }
 
     free(cmdline);
